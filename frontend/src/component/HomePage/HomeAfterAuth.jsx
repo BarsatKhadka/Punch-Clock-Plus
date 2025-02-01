baaa3aece48ab62e36a1cbf524a8bfb4880a23a3
BarsatKhadka <khadkabarsat598@gmail.com>
2025-02-01 10:23:23 -0600

    add: Jobs card in frontend

--- frontend/src/component/HomePage/HomeAfterAuth.jsx
+++ frontend/src/component/HomePage/HomeAfterAuth.jsx
@@ -1,8 +1,9 @@
+import { Jobs } from "../Jobs/Jobs";
 export const HomeAfterAuth = () => {
     return (
       <div className="flex flex-col md:flex-row h-screen w-full">
-        <div className="flex-1 bg-blue-500 flex items-center justify-center text-white text-2xl">
-          Left Side
+        <div className="flex-1 bg-blue-500 flex text-white text-2xl"> 
+         <Jobs/>
         </div>
         <div className="flex-1 bg-green-500 flex items-center justify-center text-white text-2xl">
           Right Side
---
+++ frontend/src/component/Jobs/Jobs.jsx
@@ -0,0 +1,30 @@
+import { MdOutlineSchedule } from "react-icons/md"; 
+import { HiOutlineUsers } from "react-icons/hi"; 
+
+const JobsCard = (props) => {
+    return (
+      <div className="ml-8 mt-12">
+         <span className="font-semibold"> Your Jobs</span> 
+        <div className="bg-white text-black p-3 rounded-2xl shadow-md flex flex-col gap-2 mt-8">
+          <span className="text-md font-semibold">{props.JobName}</span>
+          <p className="text-sm text-gray-600">{props.JobDescription}</p>
+          <div className="flex justify-end gap-2">
+            <button className="bg-blue-500 text-black px-3 py-1 rounded-lg text-sm flex items-center gap-1">
+              <MdOutlineSchedule className="inline mr-2 text-black" /> View Shifts
+            </button>
+            <button className="bg-green-500 text-black px-3 py-1 rounded-lg text-sm flex items-center gap-1">
+              <HiOutlineUsers className="inline mr-2 text-black" /> View Employees
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+export const Jobs = () =>{
+    return(
+        <>
+        <JobsCard JobName="step"/>
+        </>
+    )
+}
