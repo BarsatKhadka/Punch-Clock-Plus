dec27102a25e8a0e9692f4e00154ba8afd9ee3be
barsatKhadka <khadkabarsat598@gmail.com>
2025-02-25 11:54:35 -0600

    add: shiftControllers

--- backend/project/src/main/java/com/punchClock/project/Controller/ShiftController/ShiftController.java
+++ backend/project/src/main/java/com/punchClock/project/Controller/ShiftController/ShiftController.java
@@ -3,38 +3,29 @@ package com.punchClock.project.Controller.ShiftController;
 import com.punchClock.project.Modals.ShiftEntity.ShiftEntity;
 import com.punchClock.project.Service.ShiftService.createShiftService;
 import com.punchClock.project.Service.ShiftService.getShiftService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/shift")
 public class ShiftController {
 
-    private final com.punchClock.project.Service.ShiftService.getShiftService getShiftService;
-    private createShiftService Create_shiftService;
+    private final getShiftService getShiftService;
+    private final createShiftService createShiftService;
+    
     public ShiftController(createShiftService createShiftService, getShiftService getShiftService) {
-        this.Create_shiftService = createShiftService;
+        this.createShiftService = createShiftService;
         this.getShiftService = getShiftService;
     }
 
     @PostMapping("/create")
     public ShiftEntity createShift(@RequestBody ShiftEntity shiftEntity) {
-        return Create_shiftService.createShift(shiftEntity);
+        return createShiftService.createShift(shiftEntity);
     }
 
     @GetMapping("/getAllShifts")
     public List<ShiftEntity> getAllShifts() {
         return getShiftService.getAllShiftEntity();
     }
-
-
-
-
-
-
-
-
 }
